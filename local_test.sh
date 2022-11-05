ea1d829d63dba5fcd658525d5ebe605bd6b262d4
Doraemonzzz <715073608@qq.com>
2022-11-05 12:35:17 +0800

    add deit large

--- local_test.sh
+++ local_test.sh
@@ -53,7 +53,9 @@ DATA=../pvc/data
 # for ARCH in norm_vit_tiny_overlap_patch16_224_mix_softmax_1_elu_rmsnorm_glu norm_vit_tiny_overlap_patch16_224_mix_relu_elu_rmsnorm_glu
 # for ARCH in norm_vit_small_conv_patch16_224_mix_softmax_1_elu_rmsnorm_glu_h12_no_block norm_vit_small_conv_patch16_224_mix_relu_elu_rmsnorm_glu_h12_no_block
 # for ARCH in norm_vit_small_patch16_224_mix_softmax_1_elu_rmsnorm_glu_h12_no_block
-for ARCH in tnn_2d_vit_small_rpe_v8_l1_prenorm_90 tno_vit_e3g1_small_rpe_l1_90_prenorm
+# for ARCH in tnn_2d_vit_small_rpe_v8_l1_prenorm_90 tno_vit_e3g1_small_rpe_l1_90_prenorm
+# for ARCH in tnn_2d_vit_tiny_rpe_v8_l1_prenorm_90 tnn_vit_e3g1_tiny_rpe_l1_90_prenorm
+for ARCH in tno_vit_e3g1_big_rpe_l1_90_prenorm
 do
     CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
         --use_env $PROG --data-set CIFAR --batch-size $batch_size --num_workers 1 --lr 3e-3 \
