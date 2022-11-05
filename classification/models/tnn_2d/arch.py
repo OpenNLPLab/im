5484fa931b07b23ee01c518e9e5671f95cc3ba37
Doraemonzzz <715073608@qq.com>
2022-11-05 12:40:34 +0800

    add deit big tnn2d

--- classification/models/tnn_2d/arch.py
+++ classification/models/tnn_2d/arch.py
@@ -438,6 +438,39 @@ def tnn_2d_vit_small_rpe_v8_l1_prenorm_90(pretrained=False, **kwargs):
     return model
 ##### decay test
 ########## Deit small
+
+########## Deit big
+@register_model
+def tnn_2d_vit_big_rpe_v8_l1_prenorm_90(pretrained=False, **kwargs):
+    dim = 768
+    glu_dim = dim
+    rpe_dim = 32
+    num_heads = 1
+    depth = 12
+    prenorm = True
+    use_decay = True
+    gamma = 0.90
+    model = TNN2DVit(
+        patch_size=16, 
+        embed_dim=dim, 
+        num_heads=num_heads, 
+        rpe_embedding=rpe_dim,
+        rpe_act="relu",
+        glu_act="silu",
+        glu_dim=glu_dim,
+        expand_ratio=3,
+        depth=depth, 
+        use_pos=False,
+        rpe_layers=1,
+        prenorm=prenorm,
+        use_decay=use_decay,
+        gamma=gamma,
+        **kwargs
+    )
+    model.default_cfg = _cfg()
+
+    return model
+########## Deit big
 ############### model_vit
 
 ############### model_pyr
--- local_test.sh
+++ local_test.sh
@@ -55,7 +55,8 @@ DATA=../pvc/data
 # for ARCH in norm_vit_small_patch16_224_mix_softmax_1_elu_rmsnorm_glu_h12_no_block
 # for ARCH in tnn_2d_vit_small_rpe_v8_l1_prenorm_90 tno_vit_e3g1_small_rpe_l1_90_prenorm
 # for ARCH in tnn_2d_vit_tiny_rpe_v8_l1_prenorm_90 tnn_vit_e3g1_tiny_rpe_l1_90_prenorm
-for ARCH in tno_vit_e3g1_big_rpe_l1_90_prenorm
+# for ARCH in tno_vit_e3g1_big_rpe_l1_90_prenorm
+for ARCH in tnn_2d_vit_big_rpe_v8_l1_prenorm_90
 do
     CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
         --use_env $PROG --data-set CIFAR --batch-size $batch_size --num_workers 1 --lr 3e-3 \
